6c0a70683a50499848a8bac605709568c53b6fd5
MazenAtlam <mazenatef5510@gmail.com>
2024-07-20 17:02:02 +0300

    Submit task [0:1]

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A module has a Square class"""
+
+
+class Square():
+    """A class that creates a square"""
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """Initialize attributes"""
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+
+        return self.width * self.height
+
+    def PerimeterOfMySquare(self):
+        """Compute the perimeter of the square"""
+
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """Formating the square printing"""
+
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PerimeterOfMySquare())
